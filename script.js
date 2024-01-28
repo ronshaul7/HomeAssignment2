ea15947fd6f081226f3cb935623cbbba1dfe83de
ronshaul7 <ronshaul7@gmail.com>
2024-01-28 18:43:43 +0200

    Add mobile navigation icon

--- script.js
+++ script.js
@@ -1,23 +1,23 @@
+
+
 document.addEventListener('DOMContentLoaded', function() {
-    const navLinks = document.querySelectorAll('.nav-link');
+    // Get the current page URL
+    var currentUrl = window.location.href;
   
-    navLinks.forEach(function(navLink) {
-      if (navLink.href === window.location.href) {
-        navLink.parentElement.classList.add('current');
+    // Get all the navigation links
+    var navLinks = document.querySelectorAll("nav ul li a");
+  
+    // Loop through each link and compare its href attribute with the current URL
+    navLinks.forEach(function(link) {
+      if (link.href === currentUrl) {
+        // Add a class to the link to highlight it as the current page
+        link.classList.add("active");
       }
     });
   });
+  
 
-  // Get the current page URL
-var currentUrl = window.location.href;
-
-// Get all the navigation links
-var navLinks = document.querySelectorAll(".navList a");
-
-// Loop through each link and compare its href attribute with the current URL
-navLinks.forEach(function(link) {
-  if (link.href === currentUrl) {
-    // Add a class to the link to highlight it as the current page
-    link.classList.add("active");
-  }
-});
+  function toggleNav() {
+    var navList = document.querySelector('.navList');
+    navList.classList.toggle('active');
+}
